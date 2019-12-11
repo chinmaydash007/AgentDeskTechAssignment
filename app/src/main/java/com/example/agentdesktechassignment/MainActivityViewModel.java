d5f221d3f398f76b8d5c13bd0426889083ef4f3f
Chinmay <chinmaydash11@gmail.com>
2019-12-11 11:47:36 +0530

    Final Commit

---
+++ app/src/main/java/com/example/agentdesktechassignment/MainActivityViewModel.java
@@ -0,0 +1,44 @@
+package com.example.agentdesktechassignment;
+
+import android.util.Log;
+
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+import com.example.agentdesktechassignment.Model.RiderProfile;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class MainActivityViewModel extends ViewModel {
+    JsonPlaceHolderApi jsonPlaceHolderApi;
+    private static final String TAG = "MainActivityViewModel";
+    MutableLiveData<RiderProfile> riderProfile;
+
+    public MutableLiveData<RiderProfile> getRiderProfile(JsonPlaceHolderApi jsonPlaceHolderApi) {
+        if (riderProfile == null) {
+            riderProfile = new MutableLiveData<>();
+            fetchDataFromServer(jsonPlaceHolderApi);
+        }
+        return riderProfile;
+    }
+
+    private void fetchDataFromServer(JsonPlaceHolderApi jsonPlaceHolderApi) {
+        final Call<RiderProfile> riderProfileCall = jsonPlaceHolderApi.getProfile("iranjith4/522d5b466560e91b8ebab54743f2d0fc/raw/7b108e4aaac287c6c3fdf93c3343dd1c62d24faf/radius-mobile-intern.json");
+        riderProfileCall.enqueue(new Callback<RiderProfile>() {
+            @Override
+            public void onResponse(Call<RiderProfile> call, Response<RiderProfile> response) {
+                if (!response.isSuccessful()) {
+                    Log.d(TAG, String.valueOf(response.code()));
+                }
+                riderProfile.setValue(response.body());
+            }
+
+            @Override
+            public void onFailure(Call<RiderProfile> call, Throwable t) {
+                Log.d(TAG, t.getMessage());
+            }
+        });
+    }
+}
